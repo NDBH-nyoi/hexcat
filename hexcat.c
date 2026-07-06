05d71f8ba3faf877eac18240bd41b7979251c8cd
B.Hoang <baohoang100708@gmail.com>
2026-07-06 11:15:24 +0700

    Added additional formatting flags for --stats

--- hexcat.c
+++ hexcat.c
@@ -56,6 +56,29 @@ int is_escape_char(char ch) {
     }
 }
 
+void print_help() {
+    printf("Usage: hexcat.exe [flags] <args>...\n");
+    printf("-i <arg>: File input.\n");
+    printf("-p --padding <arg>: Specify index padding. Default is 8.\n");
+    printf("-caps: Capitalizes hex characters.\n");
+    
+    printf("--stats: At the end of cat-ting, "
+        "returns a table with the frequency of each byte.\n");
+
+    printf("flags that support --stats:\n");
+    printf("    -st-p --stats-padding <arg_num>: "
+        "Specify the frequency padding. Default is 4.\n");
+    printf("    -st-sp --stats-space <arg_num>: "
+        "Specify the whitespace padding between columns. Default is 4.\n");
+    printf("    -st-c --stats-colnum <arg_num>: "
+        "Specify the number of columns. Default is 8.\n");
+    printf("    -st-v --stats-verbose: Will print hexs that have 0 occurence. "
+        "Default hides this.\n");
+    printf("    -fr --freq: Display stats table from highest to lowest frequency order.\n");
+
+    printf("-h --help: Show this list.\n");
+}
+
 //.\\test - Copy.exe 
 //.\\shorter_example.txt
 
@@ -70,6 +93,21 @@ char HELP_FLAG = 0; // false. Also the "\0" character.
 char CAPITAL_FLAG = 0;
 
 unsigned char PaddingSize = 8; // default padding size
+
+char STATS_FLAG = 0;
+char STATS_FREQ_FLAG = 0;
+char STATS_VERBOSE_FLAG = 0;
+unsigned char STATS_PADDING = 4; // Haven't done any work yet
+unsigned char STATS_SPACEPADDING = 4;
+unsigned char COLUMN_NUMBER = 8;
+
+unsigned int byteStats[256] = {0}; // each index represent the value of that byte e.g 0x10 is at index 16
+
+// Can probably do a comparision, and do a stats print with order most frequent to least frequent.
+// if you want, you can make it so bytes with no appearance doesn't get printed.
+
+
+// LOOK HERE FOR DIRECTIVES!!!
 
 int ch;
 
@@ -101,7 +139,9 @@ int main(int argc, char *argv[]) {
         else if (strcmp(argv[i], "-caps") == 0) {
             CLI_0ARGS(i, "-caps", CAPITAL_FLAG);
         }
-
+        else if (strcmp(argv[i], "--stats") == 0) {
+            CLI_0ARGS(i, "-stats", STATS_FLAG);
+        }
         else if (strcmp(argv[i], "--padding") == 0 || strcmp(argv[i], "-p") == 0) {
             if (i + 1 >= argc) { 
                 printf("Error: " "-p" " requires an argument.\n"); 
@@ -110,6 +150,39 @@ int main(int argc, char *argv[]) {
                 PaddingSize = atoi(argv[i+1]); 
                 i+=2; 
         }
+
+        else if (strcmp(argv[i], "--stats-colnum") == 0 || strcmp(argv[i], "-st-c") == 0) {
+            if (i + 1 >= argc) { 
+                printf("Error: " "-st-c" " requires an argument.\n"); 
+                return 1; 
+                } 
+                COLUMN_NUMBER = atoi(argv[i+1]); 
+                i+=2; 
+        }
+
+        else if (strcmp(argv[i], "--stats-padding") == 0 || strcmp(argv[i], "-st-p") == 0) {
+            if (i + 1 >= argc) { 
+                printf("Error: " "-st-p" " requires an argument.\n"); 
+                return 1; 
+                } 
+                STATS_PADDING = atoi(argv[i+1]); 
+                i+=2; 
+        }
+        else if (strcmp(argv[i], "--stats-space") == 0 || strcmp(argv[i], "-st-sp") == 0) {
+            if (i + 1 >= argc) { 
+                printf("Error: " "-st-space" " requires an argument.\n"); 
+                return 1; 
+                } 
+                STATS_SPACEPADDING = atoi(argv[i+1]); 
+                i+=2; 
+        }
+        else if (strcmp(argv[i], "--freq") == 0) {
+            CLI_0ARGS(i, "--freq", STATS_FREQ_FLAG);
+        }
+        else if (strcmp(argv[i], "--stats-verbose") == 0 || strcmp(argv[i], "-st-v") == 0) {
+            CLI_0ARGS(i, "-st-v", STATS_VERBOSE_FLAG);
+        }
+
         else {
             printf("Unknown command or missing arguments: %s.\n", argv[i]);
             printf("Type -h or --help for usage help.\n");
@@ -118,7 +191,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (PaddingSize > 16 || PaddingSize == 0 || PaddingSize == 1) {
-        printf("Padding size invalid or too large: %d.", PaddingSize);
+        printf("Padding size is invalid or too large: %d.", PaddingSize);
         return 1;
     }
 
@@ -131,12 +204,7 @@ int main(int argc, char *argv[]) {
     }
     
     if (HELP_FLAG == 1) {
-        printf("Usage: hexcat.exe [flags] <args>...\n");
-        printf("-i <args>: File input.\n");
-        printf("-p --padding <args>: Specify index padding. Default value is 8.\n");
-        printf("-caps: Capitalizes hex characters.\n");
-        printf("-h --help: Show this list.\n");
-
+        print_help(); // Go to print help to write additional help
         if (strcmp(inFileName, "") == 0) {
             return 1;
         }
@@ -155,7 +223,10 @@ int main(int argc, char *argv[]) {
         }
         
         printf(_FormatHexChar, ch);
-        
+
+        if (STATS_FLAG == 1) {
+            byteStats[ch]++;
+        }
 
         Buf_forChars16[Hex_Counter] = ch;
         Hex_Counter++;
@@ -172,7 +243,6 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    
     // this part is for dealing with the residue characters
     // that weren't printed
 
@@ -188,7 +258,6 @@ int main(int argc, char *argv[]) {
             printf("%c", *(Buf_forChars16+i));
         }
     }
-
     printf("\n");
 
     printf("End of file reached. Total file size: %llu B.\n", LineNum - (HexPerLine - Hex_Counter));
@@ -197,6 +266,52 @@ int main(int argc, char *argv[]) {
         printf("Input was likely incorrect: %s\n", inFileName);
     }
 
+    if (STATS_FREQ_FLAG == 1 && STATS_FLAG == 0) {
+        printf("Frequency flag --freq has to be used with --stats.\n");
+        return 1;
+    }
+
+    if (STATS_PADDING > 16 || STATS_PADDING == 0) {
+        printf("Stats padding size is invalid or too large: %d.", STATS_PADDING);
+        return 1;
+    }
+    if (STATS_SPACEPADDING > 16 || STATS_SPACEPADDING == 0) {
+        printf("Stats space padding is size invalid or too large: %d.", STATS_SPACEPADDING);
+        return 1;
+    }
+    if (COLUMN_NUMBER > 16 || COLUMN_NUMBER == 0) {
+        printf("Stats space padding is size invalid or too large: %d.", STATS_SPACEPADDING);
+        return 1;
+    }
+    if (STATS_FLAG == 1) {
+        printf("\n");
+        printf("Byte frequency: \n");
+        if (STATS_VERBOSE_FLAG == 1) {
+            for (int i = 0; i < 256; i++) {
+                if (i % COLUMN_NUMBER == 0) {
+                    printf("\n");
+                }
+                printf("%.2x: %.*u", i, STATS_PADDING, byteStats[i]);
+                printf("%*s", STATS_SPACEPADDING, "");
+            }
+        }
+        else {
+            printf("Hex with 0 occurences are hidden.\n");
+            int Hidden_ColumnCounter = 0;
+            for (int i = 0; i < 256; i++) {
+                if (Hidden_ColumnCounter % COLUMN_NUMBER == 0) {
+                    printf("\n");
+                    Hidden_ColumnCounter++;
+                }
+                if (byteStats[i] != 0) {
+                    printf("%.2x: %.*u", i, STATS_PADDING, byteStats[i]);
+                    printf("%*s", STATS_SPACEPADDING, "");
+                    Hidden_ColumnCounter++;
+                }                      
+            }
+        }
+    }
+    
     printf("\n");
     
     fclose(INPUT_FILE);
